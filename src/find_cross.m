1de92d267abd88a0fe6fa90c10f239458fdb872e
Hasith Perera <12579141+hasithperera@users.noreply.github.com>
2023-03-17 15:04:42 -0400

    fixed a couple of bugs for multiple crossing detection

--- src/find_cross.m
+++ src/find_cross.m
@@ -1,5 +1,5 @@
 
-function [indx] = find_cross(signal,thresh,side)
+function [indx] = find_cross(signal,thresh,side,debug)
 %   FIND_CROSS Summary of this function goes here
 %   finds the intersection of two lines
 %   designed to only handel curves with two intersectios
@@ -8,13 +8,32 @@ function [indx] = find_cross(signal,thresh,side)
 [min_1,min_indx] = min(signal(max_indx:end));
 temp_prof = abs(signal - thresh);
 
+if ~exist('debug','var')
+     % third parameter does not exist, so default it to something
+     debug = 0;
+end
+
+
 if (side==1)
     [min_1,min_indx] = min(temp_prof(max_indx:end));
     indx = min_indx + max_indx-1;
 else
-     [min_1,min_indx] = min(temp_prof(1:max_indx));
-     indx = min_indx;
+%      [min_1,min_indx] = min(temp_prof(1:max_indx));
+% handel up to 3 crossing points
+% fix for the original implimentation due to multiple crossings
+% 3/17/23 - ahe
+
+     [~,indx_t] = sort(temp_prof(1:max_indx));
+     [indx_t2,~] = sort(indx_t(1:6));
+     indx = indx_t2(1);
+end
+
+if (debug==1)
+   fprintf("Debug mode\n")
+   plot(signal,'o')
+   hold on 
+   plot(thresh*ones(size(temp_prof)))
+   plot(indx,signal(indx),'o')
 end
 
 end
-
--- src/process_multi.m
+++ src/process_multi.m
@@ -1,14 +1,14 @@
 close all
 clear all
 
-data_dir = 'C:\Users\remote\Desktop\Susith_data\';
-dark_frame = 'C:\Users\remote\Desktop\Susith_data\1.blank\blank _00023.jpg';
+data_dir = 'C:\RESEARCH\.1.1OH images\Susith- retake data accurate(3.12.2022)\OH height and width\5.00 mm (31 MM)';
+dark_frame = 'C:\RESEARCH\.1.1OH images\Susith - new ring,nozzle 4.2 mm\1.blank\blank _00012.jpg';
 
 
 
 %% read the images-flame,blank.
-folder = '-7.00 KV'
-dark = im2double(imread(dark_frame));
+folder = '-6.25 KV'
+dark = imread(dark_frame);
 
 
 
@@ -19,21 +19,21 @@ files = dir(strcat(data_dir,'/',folder,'/*.jpg'));
 box = [737 1361 353 1296];
 
 low_thresh = .1;
-high_thresh = .2;
+high_thresh = .1;
 
 %% loop for all images
 
-fp = fopen(strcat('ahe_',folder,'.csv'),'a');
+fp = fopen(strcat('_',folder,'.csv'),'a');
 
 for i = 1:length(files)
     
     current_file = strcat(files(i).folder,'\',files(i).name);
-    II = imread(current_file);
+%     II = imread(current_file);
     
-    
-    im_current = im2double(imread(current_file));
+    disp(files(i).name)
+    im_current = imread(current_file);
     imgDiff = abs(im_current - dark);
-    I = imgaussfilt(imgDiff,2);
+    I = imgaussfilt(imgDiff,7);
     roi = I(box(3):box(4),box(1):box(2));
 
     
@@ -42,10 +42,14 @@ for i = 1:length(files)
     baseline = mean(sig(end-100:end));
     s1 = sig-baseline;
     [max_sig,max_indx] = max(s1);
+    disp(max_sig)
     low_flame_lim = max_sig * low_thresh;
     max_flame_lim = max_sig * high_thresh;
+    
+    disp(low_flame_lim)
     i_l = find_cross(s1,low_flame_lim,1);
-    i_h = find_cross(s1,max_flame_lim,0);
+    
+    i_h = find_cross(s1,max_flame_lim,0,1);
     
     sig = sum(roi);
     baseline = mean(sig(end-100:end));
@@ -53,11 +57,13 @@ for i = 1:length(files)
     [max_sig,max_indx] = max(s1);
     low_flame_lim = max_sig * low_thresh;
     max_flame_lim = max_sig * high_thresh;
-    i_l_w = find_cross(s1,low_flame_lim,1);
+    i_l_w = find_cross(s1,low_flame_lim,0);
     i_h_w = find_cross(s1,max_flame_lim,0);
     fn_id = split(files(i).name,'_');
     
     fprintf(fp,'%s,%d,%d,%d,%d\n',fn_id{1},i_l,i_h,i_l_w,i_h_w);
+    fprintf(2,'%s,%d,%d,%d,%d\n',fn_id{1},i_l,i_h,i_l_w,i_h_w);
     fprintf("%d - ok\n",i)
     
 end
+fclose(fp)
